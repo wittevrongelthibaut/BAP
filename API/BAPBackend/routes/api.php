f3afc066fa6ec6a13b75ee6875523f82398c614a
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-02-23 13:32:19 +0100

    change the routes to add a /

--- API/BAPBackend/routes/api.php
+++ API/BAPBackend/routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout');
+    Route::post('/refresh', 'refresh');
 
 });
