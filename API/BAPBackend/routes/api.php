8378aeb2cb85c512b02f35665dfa40a327a2a892
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-02 13:38:13 +0100

    create route to retrieve menus

--- API/BAPBackend/routes/api.php
+++ API/BAPBackend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\MenuController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
@@ -16,4 +17,12 @@ Route::controller(RecipeController::class)->group(function () {
     Route::get('/recipes', 'all');
     Route::get('/recipes/{id}', 'retrieveRecipeById');
     Route::get('/recipes/random/{limit?}', 'retrieveRandomRecipes');
+});
+
+
+
+Route::group(['middleware'=> ['isauth']], function(){
+    Route::controller(MenuController::class)->group(function () {
+        Route::get('/menus', 'all');
+    });
 });
