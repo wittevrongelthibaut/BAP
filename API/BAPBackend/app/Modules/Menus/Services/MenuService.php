f87bc8d83493c699484ad5a80372706a30f41f64
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-02 15:31:23 +0100

    add functionality to create a menu

--- API/BAPBackend/app/Modules/Menus/Services/MenuService.php
+++ API/BAPBackend/app/Modules/Menus/Services/MenuService.php
@@ -4,12 +4,14 @@ namespace App\Modules\Menus\Services;
 
 use App\Modules\Core\Services\Service;
 use App\Models\Menu;
+use Error;
+use Illuminate\Support\Facades\DB;
 
 class MenuService extends Service{
 
     protected $rules = [
         'user_id' => 'required|integer|exists:App\Models\User,id',
-        'title' => 'required|string',
+        'name' => 'required|string',
         'recipes' => 'required|array',
         'recipes.*' => 'required|integer|exists:App\Models\Recipe,id'
     ];
@@ -25,15 +27,23 @@ class MenuService extends Service{
     }
 
     public function create($data){
-
+        
         $data['user_id'] = $data['user']->id;
         unset($data['user']);
-
+        
         $this->validate($data);
         if($this->hasErrors()){
             return;
         }
-        
-        dd($data);
+        DB::beginTransaction();
+        try{
+            $menu = $this->model->create($data);
+            $menu->recipes()->attach($data['recipes']);
+            DB::commit();
+            return $menu;
+        }catch(Error $e){
+            DB::rollback();
+            $this->addError($e->getMessage());
+        }
     }
 }
