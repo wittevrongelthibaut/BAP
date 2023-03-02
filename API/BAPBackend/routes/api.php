6ab83a656eebb8aef7e2d37f34bcf51b3037cd4f
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-02 11:13:46 +0100

    create route to retrieve recipes

--- API/BAPBackend/routes/api.php
+++ API/BAPBackend/routes/api.php
@@ -3,11 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
     Route::post('/logout', 'logout');
     Route::post('/refresh', 'refresh');
+});
 
+Route::controller(RecipeController::class)->group(function () {
+    Route::get('/recipes', 'all');
 });
