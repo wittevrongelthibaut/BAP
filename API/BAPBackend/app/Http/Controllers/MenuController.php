7acbfdb4266ce992b30699b66b6e6f986374c1cf
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-02 14:17:16 +0100

    create function to create a menu

--- API/BAPBackend/app/Http/Controllers/MenuController.php
+++ API/BAPBackend/app/Http/Controllers/MenuController.php
@@ -4,19 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Modules\Menus\Services\MenuService;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use App\Modules\Core\Services\ResponseService;
 
 class MenuController extends Controller
 {
     private $menuService;
+    private $responseService;
 
-    public function __construct(MenuService $menuService)
+    public function __construct(MenuService $menuService, ResponseService $responseService)
     {
         $this->menuService = $menuService;
+        $this->responseService = $responseService;
     }
 
     public function all(Request $request){
         $userId = $request->user->id;
         return $this->menuService->all($userId);
+    }
+
+    public function create(Request $request){
+        $data = $request->all();
+        $this->menuService->create($data);
+
+        if($this->menuService->hasErrors()){
+            return $this->responseService->statusBadResponse('Could not create menu', $this->menuService->getErrors());
+        }
+
+        return $this->responseService->statusOkResponse('Menu created');
     }
 }
--- API/BAPBackend/app/Modules/Menus/Services/MenuService.php
+++ API/BAPBackend/app/Modules/Menus/Services/MenuService.php
@@ -7,6 +7,13 @@ use App\Models\Menu;
 
 class MenuService extends Service{
 
+    protected $rules = [
+        'user_id' => 'required|integer|exists:App\Models\User,id',
+        'title' => 'required|string',
+        'recipes' => 'required|array',
+        'recipes.*' => 'required|integer|exists:App\Models\Recipe,id'
+    ];
+
     public function __construct(Menu $model)
     {
         parent::__construct($model);
@@ -15,5 +22,18 @@ class MenuService extends Service{
     public function all($userId)
     {
         return $this->model->where('user_id', $userId)->get();
+    }
+
+    public function create($data){
+
+        $data['user_id'] = $data['user']->id;
+        unset($data['user']);
+
+        $this->validate($data);
+        if($this->hasErrors()){
+            return;
+        }
+        
+        dd($data);
     }
 }
