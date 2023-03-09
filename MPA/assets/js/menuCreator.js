dec45d8ac3329ddf8003d789de51ba8e421b7df3
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-09 14:10:45 +0100

    add functionality to retrieve queryParams

--- MPA/assets/js/menuCreator.js
+++ MPA/assets/js/menuCreator.js
@@ -1,2 +1,33 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    console.log(retrieveQueryParameters());
+}
+
+function retrieveQueryParameters(){
+    const rawParameters = splitQueryParameters();
+    return createParameterObject(rawParameters);
+
+}
+
+function splitQueryParameters(){
+    const query = window.location.search.substring(1);
+    const vars = query.split("&");
+    return vars;
+}
+
+function createParameterObject(rawParameters){
+    let parameterObject = {
+        mealtime: [],
+        amount: 0
+    };
+
+    rawParameters.forEach(param => {
+        const [key, value] = param.split('=');
+        if (key === 'mealtime') parameterObject.mealtime.push(value);
+        else parameterObject.amount = value;
+      });
+    return parameterObject;
+}
