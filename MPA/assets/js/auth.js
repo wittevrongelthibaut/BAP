7b0a57bee23beeab1a001902808ef4abfeeda66b
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-09 08:14:34 +0100

    add method to register

--- MPA/assets/js/auth.js
+++ MPA/assets/js/auth.js
@@ -3,13 +3,37 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-    document.querySelector('input[type="submit"]').addEventListener('click', login);
+   setClickEvents();
+}
+
+function setClickEvents(){
+    if(window.location.pathname == "/login.html"){
+        document.querySelector('input[type="submit"]').addEventListener('click', login);
+    } 
+    else
+    {
+        document.querySelector('input[type="submit"]').addEventListener('click', register);
+    }
 }
 
 function login(e){
     e.preventDefault();
     let formdata = new FormData(document.querySelector('form'));
-    APIlogin(formdata)
+    APIauthentication(formdata, "/login")
+    .then(data => {
+        if(data.status == "success"){
+            saveLoginResponse(data);
+            window.location.href = "index.html";
+        } else {
+            console.log(data);
+        }
+    });
+}
+//TODO see if login and register can be combined into one function
+function register(e){
+    e.preventDefault();
+    let formdata = new FormData(document.querySelector('form'));
+    APIauthentication(formdata, "/register")
     .then(data => {
         if(data.status == "success"){
             saveLoginResponse(data);
