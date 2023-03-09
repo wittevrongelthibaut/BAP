f7d4b29543fbdcbfad3056ae5e786cbdbf8c163c
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-09 08:14:26 +0100

    change the login method

--- MPA/assets/js/api.js
+++ MPA/assets/js/api.js
@@ -2,11 +2,11 @@
 
 const API_URL = "http://bapbackend.local/api";
 
-async function APIlogin(formdata){
-    return await fetch(API_URL + "/login", {
+async function APIauthentication (formdata, slug){
+    return await fetch(API_URL + slug, {
         method: "POST",
         body: formdata
     })
     .then(response => response.json())
     .then(data => { return data; });
-};
+}
