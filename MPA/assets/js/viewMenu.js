b47f760a4d4ba11204b55d845b9ca9da03fe87f5
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-19 20:23:23 +0100

    add eventlistener

--- MPA/assets/js/viewMenu.js
+++ MPA/assets/js/viewMenu.js
@@ -10,11 +10,16 @@ async function init(){
     await setRecipes();
     createRecipeCards(recipes);
     removeLoading();
-    removeHiddenClass();
+    removeHiddenClass(true);
 }
 
 async function setRecipes(){
     const urlParams = new URLSearchParams(window.location.search);
     const menu = urlParams.get('id');
     recipes = await APIgetMenuRecipes(menu);
+    document.querySelector("button").addEventListener("click", () => navToIngredients(menu));
+}
+
+function navToIngredients(menuId){
+    window.location.href = `ingredients.html?menuid=${menuId}`;
 }
