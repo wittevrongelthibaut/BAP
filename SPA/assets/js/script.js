d762826daa248397f269bd5d29a060073a419e50
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-20 15:47:32 +0100

    remove showloginform declaration from script

--- SPA/assets/js/script.js
+++ SPA/assets/js/script.js
@@ -6,12 +6,3 @@ document.addEventListener("DOMContentLoaded", init);
 function init(){
     document.querySelector("header > a:last-of-type").addEventListener("click", showLoginForm);
 }
-
-function showLoginForm(e){
-    e.preventDefault();
-    console.log('hi');
-    const loginFormTemplate = document.querySelector("#loginFormTemplate");
-    const loginFormHtml = loginFormTemplate.content.cloneNode(true);
-
-    document.querySelector("main").appendChild(loginFormHtml);
-}
