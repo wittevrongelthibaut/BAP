26f1a0421deb48e387862bcc78eb7ba60f6ab00d
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-20 16:40:36 +0100

    add showProfileScreen

--- SPA/assets/js/script.js
+++ SPA/assets/js/script.js
@@ -5,4 +5,20 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init(){
     document.querySelector("header > a:last-of-type").addEventListener("click", showLoginForm);
+    document.querySelector('#mealGen input[type="submit"]').addEventListener("click", generateMenu);
+}
+
+function generateMenu(e){
+    e.preventDefault();
+    const formdata = new FormData(document.querySelector('form'));
+    const urlParams = new URLSearchParams(formdata);
+    window.history.pushState({}, "", `?${urlParams}`);
+    clearMain();
+}
+
+function showProfileScreen(){
+    clearMain();
+    const profileTemplate = document.querySelector("#profileTemplate");
+    const profileHtml = profileTemplate.content.cloneNode(true);
+    document.querySelector("main").appendChild(profileHtml);
 }
