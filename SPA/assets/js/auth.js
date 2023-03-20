c9458951f123fdaed0876ad7926b972cb98f3a54
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-20 16:40:22 +0100

    make it possible to login

--- SPA/assets/js/auth.js
+++ SPA/assets/js/auth.js
@@ -5,6 +5,7 @@ function showLoginForm(){
     const loginFormTemplate = document.querySelector("#loginFormTemplate");
     const loginFormHtml = loginFormTemplate.content.cloneNode(true);
     loginFormHtml.querySelector("a").addEventListener("click", showRegisterForm);
+    loginFormHtml.querySelector('input[type="submit"]').addEventListener('click', login);
 
     document.querySelector("main").appendChild(loginFormHtml);
 }
@@ -15,4 +16,24 @@ function showRegisterForm(){
     const registerFormHtml = registerFormTemplate.content.cloneNode(true);
 
     document.querySelector("main").appendChild(registerFormHtml);
+}
+
+function login(e){
+    e.preventDefault();
+    let formdata = new FormData(document.querySelector('form'));
+    APIauthentication(formdata, "/login")
+    .then(data => {
+        if(data.status == "success"){
+            saveLoginResponse(data);
+            showProfileScreen();
+            window.history.pushState({}, "", `#profile`);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+function saveLoginResponse(data){
+    localStorage.setItem('token', data.authorisation.token);
+    localStorage.setItem('user', JSON.stringify(data.user));
 }
