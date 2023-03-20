297cdb397d8b39957aa2601581b0bd990c6d3824
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-20 16:03:30 +0100

    add function to show register form

--- SPA/assets/js/auth.js
+++ SPA/assets/js/auth.js
@@ -4,6 +4,15 @@ function showLoginForm(){
     clearMain();
     const loginFormTemplate = document.querySelector("#loginFormTemplate");
     const loginFormHtml = loginFormTemplate.content.cloneNode(true);
+    loginFormHtml.querySelector("a").addEventListener("click", showRegisterForm);
 
     document.querySelector("main").appendChild(loginFormHtml);
+}
+
+function showRegisterForm(){
+    clearMain();
+    const registerFormTemplate = document.querySelector("#registerFormTemplate");
+    const registerFormHtml = registerFormTemplate.content.cloneNode(true);
+
+    document.querySelector("main").appendChild(registerFormHtml);
 }
