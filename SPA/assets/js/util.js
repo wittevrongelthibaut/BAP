b9335dfd5ffe35d70af3a8c7e9dd33950ca87bfe
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-20 16:41:44 +0100

    add method to see if user is logged in

--- SPA/assets/js/util.js
+++ SPA/assets/js/util.js
@@ -4,3 +4,18 @@ function clearMain(){
     const main = document.querySelector("main");
     main.innerHTML = "";
 }
+
+function checkLoggedIn(){
+    if (localStorage.getItem("user") === null){
+        return false;
+    }
+    return true;
+}
+
+function replaceLoginWithUser(){
+    if(checkLoggedIn()){
+        const user = JSON.parse(localStorage.getItem("user"));
+        document.querySelector("header > a:last-of-type").innerHTML = user.name;
+        document.querySelector("header > a:last-of-type").href = "profile.html";
+    }
+}
