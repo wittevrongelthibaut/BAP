4af38c6e8630c534a32644c29fc4e2d8761548d2
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-21 20:07:16 +0100

    add functions to show saved menus

---
+++ SPA/assets/js/savedMenus.js
@@ -0,0 +1,33 @@
+"use strict";
+
+async function initialiseSavedMenus(){
+    replaceLoginWithUser();
+    document.querySelector("#backwards").addEventListener("click", navigateBackInHistory);
+    insertLoading('beforeend', 'main');
+    replaceLoginWithUser();
+    await fillSavedMenus();
+    removeLoading();
+}
+
+async function fillSavedMenus() {
+    const savedMenus = await APIgetSavedMenus();
+    createSavedMenus(savedMenus);
+}
+
+function createSavedMenus(savedMenus){
+    savedMenus.forEach(menu => {
+        const savedMenuTemplate = document.querySelector('#savedMenuTemplate');
+        const savedMenuHTML = savedMenuTemplate.content.cloneNode(true);
+
+        savedMenuHTML.querySelector('article').dataset.id = menu.id;
+        savedMenuHTML.querySelector('h3').innerHTML = menu.name;
+        savedMenuHTML.querySelector('p').innerHTML = menu.created_at;
+        savedMenuHTML.querySelector('span').addEventListener('click', () => navigateToMenu(menu.id));
+
+        document.querySelector(`main`).appendChild(savedMenuHTML);
+    });
+}
+
+function navigateToMenu(id) {
+    window.location.href = `menu.html?id=${id}`;
+}
