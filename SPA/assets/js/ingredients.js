21a597ffc0343a6cb67e21612fe09be353468c95
Thibaut Wittevrongel <thibaut.wittevrongel@student.howest.be>
2023-03-21 21:02:29 +0100

    add ingredients.js

---
+++ SPA/assets/js/ingredients.js
@@ -0,0 +1,67 @@
+"use strict";
+
+async function initialiseIngredients(){
+    clearMain();
+    insertIngredientsTemplate();
+    document.querySelector("#backwards").addEventListener("click", navigateBackInHistory);
+    insertLoading('beforeend', 'main')
+    replaceLoginWithUser();
+    await insertIngredients();
+    document.querySelector("button").addEventListener("click", () => downloadIngredients());
+    removeLoading();
+    removeHiddenClass(true);
+}
+
+async function insertIngredients(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const menu = urlParams.get('menuid');
+    const ingredients = await APIgetMenuIngredients(menu);
+    console.log(ingredients);
+    createListsFromIngredients(ingredients);
+}
+
+function createListsFromIngredients(obj) {
+    for (const key in obj) {
+      const mealtime = obj[key];
+    
+      if (mealtime.length === 0) continue;
+      
+      const article = document.querySelector(`#${key}`);
+      const list = document.createElement("ul");
+      list.classList.add("ingredientsList");
+      
+      createLiForUl(mealtime, list);
+
+      article.appendChild(list);
+    }
+}
+
+function createLiForUl(mealtime, list){
+    for (const item of mealtime) {
+        const listItem = document.createElement("li");
+        listItem.innerText = item;
+        list.appendChild(listItem);
+      }
+}
+
+function downloadIngredients(){
+    const main = document.querySelector("main");
+    const textContent = main.innerText;
+
+    const blob = new Blob([textContent], { type: 'text/plain' });
+
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'ingredients.txt';
+    
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function insertIngredientsTemplate(){
+    const ingredientsTemplate = document.querySelector("#ingredientTemplate");
+    const ingredientsHtml = ingredientsTemplate.content.cloneNode(true);
+    document.querySelector("main").appendChild(ingredientsHtml);
+}
+  
